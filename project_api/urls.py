d5a7634fba555e2d54e81f657e168e2e56ca778d
Leonard <leonardbinet@gmail.com>
2017-02-17 22:50:25 +0100

    Feature: api in menu

--- project_api/urls.py
+++ project_api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from project_api import views
 
 urlpatterns = [
-    url(r'^station/$', views.GetStationDisplayedTrains.as_view()),
+    url(r'^station/$', views.GetStationDisplayedTrains.as_view(), name='api'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
