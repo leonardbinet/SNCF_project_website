cb1825ecdeb498422a12250ee7c3e4032a1b3e42
Leonard <leonardbinet@gmail.com>
2017-02-27 20:55:18 +0100

    Settings: minor change

--- sncfweb/settings/prod.py
+++ sncfweb/settings/prod.py
@@ -4,10 +4,3 @@ from .base import BASE_DIR
 import os
 
 DEBUG = True
-
-# Endroit ou ce sera stocké sur le serveur
-# Soit cela est spécifié dans les variables d'environnment, soit on le
-# stocke dans un répertoire un niveau au dessus puis dans static
-STATIC_ROOT = os.environ.get('static', None)
-if not STATIC_ROOT:
-    STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
