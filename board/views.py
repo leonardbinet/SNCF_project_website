57fba6f6a7e6c9a0403a4c64a9ea91288313a189
Leonard <leonardbinet@gmail.com>
2017-05-05 14:23:02 +0200

    Board view

--- board/views.py
+++ board/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import pandas as pd
 
 
 def trip(request):
@@ -7,5 +8,23 @@ def trip(request):
 
 
 def station(request):
+    """This view shows two things:
+    \n- a table describing possible stations
+    """
+    gares = pd.read_csv(
+        "data/sncf-gares-et-arrets-transilien-ile-de-france.csv", sep=";")
+    cols = [
+        'Code UIC', 'uic7', "Libelle point d'arret", 'Libelle',
+        'Libelle STIF (info voyageurs)', 'Libelle SMS gare', 'Nom Gare',
+        'Adresse', 'Code INSEE commune', 'Commune', 'X (Lambert II etendu)',
+        'Y (Lambert II etendu)', 'Coord GPS (WGS84)', 'Zone Navigo',
+        'Gare non SNCF'
+    ]
+    cols_keep = [
+        'Code UIC', 'uic7', "Libelle point d'arret", 'Zone Navigo',
+        'Gare non SNCF'
+    ]
+    gares = gares[cols_keep].applymap(str)
+    gares = gares.to_dict(orient="record")
     context = {"type": "station"}
     return render(request, 'board/station.html', context)
