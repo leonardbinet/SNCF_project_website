3f64fa2d99079f24313116c715c598c14223092b
Leonard <leonardbinet@gmail.com>
2017-06-19 09:55:02 +0200

    View cleaning

--- project_api/views.py
+++ project_api/views.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from distutils.util import strtobool
 
 from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework import generics
 
 from api_etl.query import DBQuerier
