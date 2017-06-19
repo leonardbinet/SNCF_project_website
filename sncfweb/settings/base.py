f413839a8430bbfd84fb16022a33e1925e1a21e3
Leonard <leonardbinet@gmail.com>
2017-06-19 11:25:06 +0200

    Settings cleanup

--- sncfweb/settings/base.py
+++ sncfweb/settings/base.py
@@ -201,6 +201,7 @@ CACHES = {
 CACHE_MIDDLEWARE_SECONDS = 60
 
 
+# STATIC FILES
 # Endroit ou ce sera stocké sur le serveur
 # Soit cela est spécifié dans les variables d'environnment, soit on le
 # stocke dans un répertoire un niveau au dessus puis dans static
@@ -210,9 +211,8 @@ if not STATIC_ROOT:
 
 WSGI_APPLICATION = 'sncfweb.wsgi.application'
 
-# Python crashes or captured as well (beware of ipdb imports)
 
-
+# Python crashes or captured as well by logging
 def handle_exception(exc_type, exc_value, exc_traceback):
     # if issubclass(exc_type, KeyboardInterrupt):
     #    sys.__excepthook__(exc_type, exc_value, exc_traceback)
@@ -221,43 +221,3 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     sys.__excepthook__(exc_type, exc_value, exc_traceback)
 
 sys.excepthook = handle_exception
-
-
-logs_path = os.path.join(BASE_DIR, "..", "logs")
-
-
-# ##### DATA PATH #####
-data_path = os.path.join(BASE_DIR, "data")
-gtfs_path = os.path.join(data_path, "gtfs-lines-last")
-
-gtfs_csv_url = 'https://ressources.data.sncf.com/explore/dataset/sncf-transilien-gtfs/'\
-    + 'download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true'
-
-# Stations files paths
-responding_stations_path = path.join(data_path, "responding_stations.csv")
-top_stations_path = path.join(data_path, "most_used_stations.csv")
-scheduled_stations_path = path.join(
-    data_path, "scheduled_station_20170215.csv")
-all_stations_path = path.join(data_path, "all_stations.csv")
-
-
-# ##### DATABASES #####
-
-# Sqlite
-sqlite_path = os.path.join(BASE_DIR, "schedules.db")
-
-# Mongo DB collections:
-col_real_dep_unique = "real_departures_2"
-
-# Dynamo DB tables:
-dynamo_real_dep = "real_departures_2"
-dynamo_sched_dep = "scheduled_departures"
-dynamo_sched_dep_all = "scheduled_departures_all"
-
-# Dynamo provisioned_throughput for tables
-real_read = 50
-real_write = 80
-# 'on' is for when we update, 'off' rest of the time
-shed_read = 100
-shed_write_off = 1
-shed_write_on = 100
